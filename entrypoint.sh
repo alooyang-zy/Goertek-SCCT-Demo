594e3e5a18551e57262396c81891d0256f5f5613
alooyang-zy <alooyang-zy@users.noreply.github.com>
2026-06-21 11:09:37 +0800

    fix: 简化 nginx 为纯 HTTP 模式，修复 SSL 证书问题

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
-mkdir -p /etc/nginx/certs
-if [ -f /certs/server.crt ] && [ -f /certs/server.key ]; then
-    cp /certs/server.crt /etc/nginx/certs/server.crt
-    cp /certs/server.key /etc/nginx/certs/server.key
-    echo "Using Let's Encrypt cert"
-else
-    echo "WARNING: No cert found, nginx may fail"
-fi
+echo "Starting Goertek SCCT Demo..."
 exec nginx -g "daemon off;"
